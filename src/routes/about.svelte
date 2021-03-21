aae3032f1651936a1a015853115814d4e467991e
Christo-IV <christo.vahtramae@khk.ee>
2021-03-21 12:34:15 +0200

    Sapper file and something else that I no longer remember

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -7,4 +7,4 @@
 <input type="text" placeholder="Username" />
 <input type="text" placeholder="Password" />
 <button>Log in</button>
-<p>Don't have an account? Create one <a href="#">here</a></p>
+<p>Don't have an account? Create one <a href="register.svelte">here</a></p>
