2e58c4d2f5b71bc9d327a960d2806cc4dab8809b
SanderPaasalu <sander.paasalu@khk.ee>
2021-03-21 15:33:34 +0200

    Made some changes and fixes to login page to prep it for when we get some toher stuff to work

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,10 +1,20 @@
+<script>
+let username;
+let password;
+
+function loginHandler() {
+
+}
+</script>
+
 <svelte:head>
   <title>Log in</title>
 </svelte:head>
 
 <h1>Log in</h1>
-
-<input type="text" placeholder="Username" />
-<input type="text" placeholder="Password" />
-<button>Log in</button>
+<form>
+  <input bind:value={username} type="text" placeholder="Username" />
+  <input bind:value={password} type="text" placeholder="Password" />
+  <button on:click={loginHandler} type="submit">Log in</button>
+</form>
 <p>Don't have an account? Create one <a href="./register">here</a></p>
